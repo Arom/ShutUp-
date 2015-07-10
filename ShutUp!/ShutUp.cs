22ef4aa396deedd5d3ce5a7156dc97753bf25cc2
Arom <aromchris@hotmail.com>
2015-07-10 20:20:01 +0100

    Introduced threading, no more 15 min warnings before shutdown.

---
+++ ShutUp!/ShutUp.cs
@@ -0,0 +1,42 @@
+﻿/*
+        ShutUp! v .0.2 - Shuts down the PC using shutdown.exe provided with Windows.
+        Developed becasue I was too lazy to get up in the middle of the night to shut down
+        my computer when I'm finished watching movies.
+ 
+        Copyright (C) 2014 Christopher Ilkow
+
+        This program is free software: you can redistribute it and/or modify
+        it under the terms of the GNU General Public License as published by
+        the Free Software Foundation, either version 3 of the License, or
+        (at your option) any later version.
+
+        This program is distributed in the hope that it will be useful,
+        but WITHOUT ANY WARRANTY; without even the implied warranty of
+        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+        GNU General Public License for more details.
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Diagnostics;
+namespace ShutUp_
+{
+    class ShutUp
+    {
+        public void StartShutdown(int ms)
+        {
+            Thread.Sleep(ms);
+            Console.WriteLine("ohai");
+            Process.Start("shutdown.exe", "-s -t " + ms * 1000);
+        }
+        public void StopShutdown()
+        {
+            Process.Start("shutdown.exe ", "-a");
+            Console.WriteLine("bye");
+        }
+    }
+}
